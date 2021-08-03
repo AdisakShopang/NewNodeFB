f5a61575ea88538e2c70b78596ba9ee822cd171f
AdisakTovititwongShopang <ato@manaosoftware.com>
2021-08-03 18:33:55 +0700

    Deploy success then call able api

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,16 +1,29 @@
 // LOCALHOST SERVER
 import * as functions from "firebase-functions";
 import { apiRouter } from './routers'
+import { Request, Response } from 'express';
 
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 8080;
+// const PORT = process.env.PORT || 8080
 
 app.use(apiRouter);
+app.all('*', (req: Request, res: Response) => {
+  const code: number = 404;
+  const result: any = {
+      status_code: code,
+      success: false,
+      error: {
+          message: "Req not found"
+      }
+  };
+  // customLogging(req, result)
+  return res.status(code).send(result);
+})
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port : ${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port : ${PORT}`);
+// });
 
 // import { Request, Response } from "express";
 
